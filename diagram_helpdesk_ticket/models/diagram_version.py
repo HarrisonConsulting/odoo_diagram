a074f5211da2528f67c5822b7ad0ffa364c45567
Matt Harrison <matt@harrison.consulting>
2025-03-30 18:34:55 -0400

    [ADD] Implement diagram version management for knowledge articles and project tasks

--- diagram_helpdesk_ticket/models/diagram_version.py
+++ diagram_helpdesk_ticket/models/diagram_version.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 
-
 class DiagramVersion(models.Model):
     _inherit = "diagram.version"
     
-    ticket_id = fields.Many2one('helpdesk.ticket')
-
-    @api.model
-    def create(self, vals):
-        new_record = super(DiagramVersion, self).create(vals)
-        ticket_id = vals.get('ticket_id') or new_record.ticket_id.id
-        # Get all records with the same ticket_id, ordered by creation date
-        # (or any field to define "latest")
-        existing_records = self.search([('ticket_id', '=', ticket_id)],
-                                       order='create_date desc')
-        limit = int(self.env['ir.config_parameter'].sudo().get_param('base_draw_io.diagram_history_records_count'))
-        # If there are more than 13 records, delete the older ones
-        if len(existing_records) > limit:
-            records_to_delete = existing_records[limit:]  # All records after the 13th latest
-            records_to_delete.sudo().unlink()
-        return new_record
+    ticket_id = fields.Many2one('helpdesk.ticket', string="Helpdesk Ticket",
+                               help="The helpdesk ticket this diagram is related to.")
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        # Pre-process values to set reference_model and reference_id
+        for vals in vals_list:
+            if vals.get('ticket_id') and not vals.get('reference_model'):
+                vals['reference_model'] = 'helpdesk.ticket'
+                vals['reference_id'] = vals['ticket_id']
+                
+        return super(DiagramVersion, self).create(vals_list)
--- diagram_knowledge_article/models/knowledge_article.py
+++ diagram_knowledge_article/models/knowledge_article.py
@@ -12,15 +12,13 @@ class KnowledgeArticle(models.Model):
         """
     _inherit = 'knowledge.article'
 
-    diagram = fields.Char(string=_("Diagram"), copy=False,
-                          help="Stores the name of the diagram associated with the article.")
-    diagram_version_id = fields.Many2one("diagram.version",
-                                         string=_("Diagram Versions"),
-                                         domain="[('article_id','=', id)]",
-                                         help="Links the diagram versions to this knowledge article."
-                                         )
-    show_load_diagram = fields.Boolean(string='Show Diagram',
-                                       help="Indicates whether to display the diagram for this article.")
+    diagram = fields.Char(string="Diagram", copy=False,
+                         help="Stores the XML representation of the diagram for this article.")
+    diagram_version_id = fields.Many2one("diagram.version", string="Diagram Versions", 
+                                       domain="[('reference_model', '=', 'knowledge.article'), ('reference_id', '=', id)]",
+                                       help="Reference to previous versions of this diagram.")
+    show_load_diagram = fields.Boolean(string="Show Load Diagram Button",
+                                     help="Technical field to control visibility of the load diagram button.")
 
     def write(self, vals):
         """ Override the write method to handle diagram saving and versioning.
@@ -28,21 +26,22 @@ class KnowledgeArticle(models.Model):
             If a new diagram is provided and 'save_diagram' is set, this method:
                 - Creates a new diagram version record.
                 - Sets a timestamped diagram name. """
-        if vals.get('diagram', False) and vals.get('save_diagram', False):
+        if vals.get('diagram') and vals.get('save_diagram'):
             del vals['save_diagram']
-            timezone = self._context.get(
-                'tz') or self.env.user.partner_id.tz or 'UTC'
-            self_tz = self.with_context(tz=timezone)
-            date = fields.Datetime.context_timestamp(self_tz,
-                                                     fields.Datetime.now())
-            diagram_name = _(
-                f"{date.strftime('%Y-%m-%d %H:%M:%S')} - {self.name}")
-            # Store the saved diagram as a record to future use
-            self.env["diagram.version"].create({
-                'name': diagram_name,
-                'diagram_xml': vals.get('diagram'),
-                'article_id': self.id,
-            })
+            for record in self:
+                timezone = self._context.get('tz') or self.env.user.partner_id.tz or 'UTC'
+                self_tz = record.with_context(tz=timezone)
+                date = fields.Datetime.context_timestamp(self_tz, fields.Datetime.now())
+                diagram_name = _(f"{date.strftime('%Y-%m-%d %H:%M:%S')} - {record.name}")
+                
+                # Create diagram version using the generic reference-based approach
+                self.env["diagram.version"].create({
+                    'name': diagram_name,
+                    'diagram_xml': vals.get('diagram'),
+                    'reference_model': 'knowledge.article',
+                    'reference_id': record.id,
+                })
+                
         return super(KnowledgeArticle, self).write(vals)
 
     @api.onchange('diagram_version_id')
@@ -53,10 +52,11 @@ class KnowledgeArticle(models.Model):
                 - Loads the diagram XML from the selected version.
                 - Sets the 'show_load_diagram' flag to True to display
                 the diagram.  """
-        if self.diagram_version_id:
-            self.diagram = self.diagram_version_id.diagram_xml
-            self.show_load_diagram = True
+        for record in self:
+            if record.diagram_version_id:
+                record.diagram = record.diagram_version_id.diagram_xml
+                record.show_load_diagram = True
 
     def update_show_load_diagram(self):
-        '''Set field show_load_diagram to Flase to hide the button '''
+        """Set field show_load_diagram to False to hide the button"""
         self.show_load_diagram = False
